b7269f2d16a61c4b70b99ba99aa7a70c8427b900
Esiarox <109488970+Esiarox@users.noreply.github.com>
2022-08-01 15:08:08 -0300

    Inicio experiencia

--- Final_SosaRodrigo/src/app/app.module.ts
+++ Final_SosaRodrigo/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LogosComponent } from './components/logos/logos.component';
 import { RedesComponent } from './components/redes/redes.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     LogosComponent,
     RedesComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
