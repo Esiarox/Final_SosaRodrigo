a20a4612b04cc6b6f8c2499121e4b24741864722
Esiarox <109488970+Esiarox@users.noreply.github.com>
2022-08-13 22:14:52 -0300

    Inicio backend

---
+++ Final_SosaRodrigo/Backend/src/test/java/com/portfolio/esiarox/ResApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.esiarox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
