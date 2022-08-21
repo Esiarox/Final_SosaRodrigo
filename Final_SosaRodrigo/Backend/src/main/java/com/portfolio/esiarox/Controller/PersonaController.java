19298855e4f56dafb3b39577581000f4b8798ffe
Esiarox <109488970+Esiarox@users.noreply.github.com>
2022-08-21 15:00:12 -0300

    Control acceso usuario

--- Final_SosaRodrigo/Backend/src/main/java/com/portfolio/esiarox/Controller/PersonaController.java
+++ Final_SosaRodrigo/Backend/src/main/java/com/portfolio/esiarox/Controller/PersonaController.java
@@ -8,6 +8,7 @@ import com.portfolio.esiarox.Entity.Persona;
 import com.portfolio.esiarox.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,18 +38,21 @@ public class PersonaController {
         return ipersonaService.buscarPersona(1);
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String crearPersona(@RequestBody Persona persona){
         ipersonaService.guardarPersona(persona);
         return "Se ha agregado correctamente";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String borrarPersona(@PathVariable Integer id){
         ipersonaService.borrarPersona(id);
         return "Se ha eliminado correctamente";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editarPersona(@PathVariable Integer id,@RequestParam("nombre") String nuevoNombre,
                                                           @RequestParam("apellido") String nuevoApellido,
